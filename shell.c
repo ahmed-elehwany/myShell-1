86fc7c3bd1efbea1340eb86a1d674370c725d78a
Muhammad Huzaifa ELAHI <melahi2@lab2-5.CS.McGill.CA>
2020-01-24 12:21:22 -0500

    User input correctly translated to words array

--- shell.c
+++ shell.c
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int parse(char string[]); // function declarations
+int parse(char string[]); // Function declarations
 
 int main(){
 	char shellBuffer[1000];
@@ -12,35 +13,36 @@ int main(){
 	printf("Welcome to the Muhammad Huzaifa Elahi shell!\n");
 	printf("Version 1.0 Created January 2020\n");
 	printf("$ ");
-	scanf("%s", shellBuffer);
-	parse(shellBuffer);
-	return 0;
+	fgets(shellBuffer, 1000, stdin);
+	shellBuffer[strlen(shellBuffer)-1] = '\0';
+	
+	return parse(shellBuffer);
 }
 
 int parse(char string[]){
 	char temp[200];
-	char *words[100];
+	char **words = calloc(100, sizeof(char*));
 
 	int inputIndex, tokenIndex;
 	int wordIndex = 0;
 
-	for(inputIndex = 0; string[inputIndex]==' ' && inputIndex < 1000; inputIndex++); // skip white spaces
+	for(inputIndex = 0; string[inputIndex]==' ' && inputIndex < 1000; inputIndex++); // Skip white spaces
 
+	// Move forward while input has not terminated
 	while(string[inputIndex] != '\0' && inputIndex < 1000){
 		
+		// Copy token from input
 		for(tokenIndex = 0; string[inputIndex] != '\0' && string[inputIndex] != ' ' && inputIndex < 1000; inputIndex++, tokenIndex++){
-
-			temp[tokenIndex] = string[inputIndex]; // extract a word
-		
+			temp[tokenIndex] = string[inputIndex]; // Extract a word
 		}
 
-		temp[tokenIndex] = '\0';
-
+		// Add string terminator to token and add token to word array
+		temp[tokenIndex] = '\0'; 
 		words[wordIndex] = strdup(temp);
 
 		inputIndex++;
 		wordIndex++;
 	}
-	printf("\nThis is the output:\n%s\n", words[0]);
+
 	return interpreter(words);
 }
